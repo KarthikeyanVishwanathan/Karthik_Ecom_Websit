1d2fb96189b6d4f49aec22dae8285a27bec32019
KarthikeyanVishwanathan <karthiknathan0604@gmail.com>
2024-11-09 20:16:19 +0530

    Login screen developed
    Login screen developed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,15 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
 import "./index.css";
+import { ToastContainer } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
     <>
       <Header />
+      <ToastContainer />
       <main className="min-h-lvh">
         <div className="container mx-auto">
           <Outlet />
